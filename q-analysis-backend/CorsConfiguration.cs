b44a79206e60f66085e33797621334919414c3bb
VladislavAlpeev <vlad.alpeev@mail.ru>
2024-06-14 22:08:52 +0300

    cors...

--- q-analysis-backend/CorsConfiguration.cs
+++ q-analysis-backend/CorsConfiguration.cs
@@ -11,7 +11,7 @@ namespace q_analysis_backend
                 options.AddPolicy("AllowSpecificOrigins", builder =>
                 {
                     builder.WithOrigins("http://localhost:3000",
-                                        "http://194.87.109.22:54321/")
+                                        "http://194.87.109.22:54321")
                            .AllowAnyHeader()
                            .AllowAnyMethod();
                 });
